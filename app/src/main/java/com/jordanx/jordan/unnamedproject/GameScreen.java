3003b175e46ccf3b8a547316e135de8eaf15df90
jordanmaurice <jordanmaurice@live.com>
2015-05-29 03:51:56 -0600

    ayy girl u beautiful

--- app/src/main/java/com/jordanx/jordan/unnamedproject/GameScreen.java
+++ app/src/main/java/com/jordanx/jordan/unnamedproject/GameScreen.java
@@ -8,10 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.DisplayMetrics;
@@ -19,56 +15,56 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Random;
-import java.util.Timer;
 
 public class GameScreen extends Activity {
     private int userScore;
     Bitmap photo;
-    private Timer t;
-    private int TimeCounter = 0;
     TextView timerText;
     TextView scoreText;
     int timerValue = 30000;
-    int timeAlloted = 30;
     final Context context = this;
-
-    CountDownTimer x = new CountDownTimer(30000, 1000) {
-        public void onTick(long millisUntilFinished) {
-            timerText.setText("" + millisUntilFinished / 1000);
-            String z = timerText.getText().toString();
-            timerValue = (Integer.parseInt(z)) * 1000;
-        }
-
-        public void onFinish() {
-            timerText.setText("" + 0);
-            updateHS(userScore);
-        }
-    }.start();
-
+    CountDownTimer mCountDownTimer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
         if (getIntent().hasExtra("byteArray")) {
             photo = BitmapFactory.decodeByteArray(
                     getIntent().getByteArrayExtra("byteArray"), 0, getIntent().getByteArrayExtra("byteArray").length);
         }
+
+        createCountDown(timerValue);
         timerText = (TextView) findViewById(R.id.timerText);
-        BitmapDrawable bdrawable = new BitmapDrawable(photo);
-        ImageButton derp = (ImageButton) findViewById(R.id.iconButton);
-        //derp.setImageBitmap(photo);
+        //BitmapDrawable bdrawable = new BitmapDrawable(photo);
+        //ImageButton derp = (ImageButton) findViewById(R.id.iconButton);
 
 
-//TIMER FOR MOVING THE BUTTON AUTOMATICALLY
+        //TIMER FOR MOVING THE BUTTON AUTOMATICALLY
         CountDownTimer z = new CountDownTimer(30000, 750) {
             public void onTick(long millisUntilFinished) {
                 moveButton();
             }
 
             public void onFinish() {
+            }
+        }.start();
+    }
+
+    public void createCountDown(int timerVal){
+       mCountDownTimer = new CountDownTimer(timerVal, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timerText.setText("" + millisUntilFinished / 1000);
+                String z = timerText.getText().toString();
+                timerValue = (Integer.parseInt(z)) * 1000;
+            }
+
+            public void onFinish() {
+                timerText.setText("" + 0);
+                updateHS(userScore);
             }
         }.start();
     }
@@ -81,11 +77,9 @@ public class GameScreen extends Activity {
             editor.putInt("highscore", x);
             editor.commit();
             Toast.makeText(getApplicationContext(), "New High Score!", Toast.LENGTH_LONG).show();
-            Intent hs = new Intent(getApplicationContext(), MainMenu.class);
-            startActivity(hs);
+            endGame();
         } else {
-            Intent hs = new Intent(getApplicationContext(), MainMenu.class);
-            startActivity(hs);
+            endGame();
         }
     }
 
@@ -99,7 +93,7 @@ public class GameScreen extends Activity {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         int width = displayMetrics.widthPixels;
         int height = displayMetrics.heightPixels;
-        ImageButton theButton = (ImageButton) findViewById(R.id.iconButton);
+        ImageButton theButton = (ImageButton) findViewById(R.id.goodIcon);
 
         Random r = new Random();
 
@@ -110,17 +104,41 @@ public class GameScreen extends Activity {
     }
 
     public void pauseClick(View a) {
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+        new AlertDialog.Builder(this)
+                .setTitle("Time Paused")
+                .setMessage("Would you like to restart the game?")
+                .setPositiveButton("Resume", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                createCountDown(timerValue);
+                                dialog.cancel();
+                            }
+                        }
+                )
+                .setNegativeButton("Leave", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent hs = new Intent(getApplicationContext(), MainMenu.class);
+                        startActivity(hs);
+                        dialog.cancel();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
+    public void endGame(){
         new AlertDialog.Builder(this)
                 .setTitle("Game Over!")
                 .setMessage("Would you like to restart the game?")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Intent hs = new Intent(getApplicationContext(), GameScreen.class);
-                        startActivity(hs);
-                        dialog.cancel();
-                    }
-                }
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent hs = new Intent(getApplicationContext(), GameScreen.class);
+                                startActivity(hs);
+                                dialog.cancel();
+                            }
+                        }
                 )
                 .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
@@ -133,7 +151,5 @@ public class GameScreen extends Activity {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
     }
-
-
 
 }
